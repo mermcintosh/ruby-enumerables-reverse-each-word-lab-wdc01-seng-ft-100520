f319a0afc39b90cf470f278c48c924c570900516
Meredith McIntosh <meredith@mcintosh.io>
2020-09-25 02:55:18 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,12 +6,15 @@
  def reverse_each_word(sentence1)
  sentence1 = sentence1.split
  reversed_string = []
- sentence1.each do |char|
-  reversed_string.unshift(char)
+
  end
  return reversed_string.join('')
 end
 p reverse_each_word("I love Chris so much!")
+
+
+# sentence1.each do |char|
+#   reversed_string.unshift(char)
 
 
   
