9585bba1d8b8d54beaf575a40d850facf30088b2
Meredith McIntosh <meredith@mcintosh.io>
2020-09-25 03:58:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,21 +3,26 @@
   #   expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
   # end
   
-def reverse_each_word(sentence1)
+# def reverse_each_word(sentence1)
+#     sentence1 = sentence1.split
+#     reversed_string = []
+#     individual_word_index = 0
+#       while individual_word_index < sentence1.count do
+#         reversed_string << sentence1[individual_word_index].reverse
+#         individual_word_index += 1
+#       end
+#         reversed_string.join(' ')
+#   end
+#   reverse_each_word("I love Chris so much!")
+  
+  def reverse_each_word(sentence1)
     sentence1 = sentence1.split
     reversed_string = []
     individual_word_index = 0
-      while individual_word_index < sentence1.count do
-        reversed_string << sentence1[individual_word_index].reverse
-        individual_word_index += 1
-      end
-        reversed_string.join(' ')
+    sentence1.each[individual_word_index].reverse_each_word
+    reversed_string.join(' ')
   end
-  reverse_each_word("I love Chris so much!")
-  
-  def reverse_each_word(sentence1)
-  end
-  reverse_each_word("I love Chris so much!")
+ p reverse_each_word("I love Chris so much!")
 
 
 a = [ "a", "b", "c" ]
