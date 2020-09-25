f4aa5b5cafc1783e41eaf3a5b613933f957e1341
Meredith McIntosh <meredith@mcintosh.io>
2020-09-25 04:24:23 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -30,7 +30,6 @@ def reverse_each_word(string)
   newArray = string.split("")
   
   newArray.collect do |word|
-    newArray = newArray.reverse
     returnedArray << word.reverse
   end
   newString = returnedArray.join(" ").to_s
