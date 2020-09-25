6eb0ea730e90fa47178c2609e2c2e29e468bb2a1
Meredith McIntosh <meredith@mcintosh.io>
2020-09-25 02:55:00 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,18 @@
+  # let(:sentence1) { "Hello there, and how are you?" }
+  # it 'reverses all the words in a string without reversing the order of the words' do
+  #   expect(reverse_each_word(sentence1)).to eq("olleH ,ereht dna woh era ?uoy")
+  # end
+  
+ def reverse_each_word(sentence1)
+ sentence1 = sentence1.split
+ reversed_string = []
+ sentence1.each do |char|
+  reversed_string.unshift(char)
+ end
+ return reversed_string.join('')
+end
+p reverse_each_word("I love Chris so much!")
+
+
+  
+  
